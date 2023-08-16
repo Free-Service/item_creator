1fa15b19e81976e30cbe8140beeac9e1a41fa87c
Sander <86840128+Sander2222@users.noreply.github.com>
2023-08-16 17:01:17 +0200

    created callback to get all items

--- server/main.lua
+++ server/main.lua
@@ -10,3 +10,18 @@ ESX.RegisterServerCallback('item_creator:callback:CheckGroup', function(src, cb)
 
   cb(false)
 end)
+
+ESX.RegisterServerCallback('item_creator:callback:GetItems', function(src, cb)
+  local xPlayer = ESX.GetPlayerFromId(src)
+  local PlayerGroup = xPlayer.getGroup()
+
+  for k, v in pairs(Config.Groups) do
+    if v == PlayerGroup then
+      MySQL.query('SELECT * FROM items', {}, function(response)
+        if response then
+          cb(response)
+        end
+      end)
+    end
+  end
+end)
