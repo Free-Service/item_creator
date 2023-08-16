6f7f848aa42911b59301519ce71f5f87540ad91f
Sander <86840128+Sander2222@users.noreply.github.com>
2023-08-16 17:01:27 +0200

    add UI (beta)

--- html/script.js
+++ html/script.js
@@ -4,8 +4,30 @@ window.addEventListener("message", async function (event) {
   var item = event.data;
 
   if (item.state === "show") {
-    ClearCreateInputs();
-  } else if (item.state === "close") {
-    $("body").hide();
+    $("body").show();
+    document.getElementsByTagName("body")[0].style.display = "flex";
+    LoadItemsinList(item.items)
   }
 });
+
+document.onkeyup = function (data) {
+  if (data.which == 27) {
+    Close();
+  }
+};
+
+function Close() {
+  $("body").hide();
+  $.post("https://item_creator/exit", JSON.stringify({}));
+}
+
+function LoadItemsinList(ItemsList) {
+  const itemListElement = document.getElementById('item-list');
+  const itemList = JSON.parse(ItemsList);
+
+  for (const item of itemList) {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${item.label} - Weight: ${item.weight}`;
+    itemListElement.appendChild(listItem);
+  }
+}
