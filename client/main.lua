57a7b3b622ec19a7af39d21c4e4a0edcfe01ad8b
Sander <86840128+Sander2222@users.noreply.github.com>
2023-08-16 17:01:34 +0200

    add callback client

--- client/main.lua
+++ client/main.lua
@@ -1,2 +1,20 @@
 RegisterCommand("item", function(source, args)
+    ESX.TriggerServerCallback('item_creator:callback:CheckGroup', function(HasGroup)
+        if HasGroup then
+            ESX.TriggerServerCallback('item_creator:callback:GetItems', function(Items)
+                -- print(ESX.DumpTable(Items))
+                SetNuiFocus(true, true)
+                SendNUIMessage({
+                    state = 'show',
+                    items = json.encode(Items)
+                })
+            end)
+        else
+            -- notify
+        end
+    end)
 end, false)
+
+RegisterNUICallback('exit', function(data, cb)
+    SetNuiFocus(false, false)
+end)
